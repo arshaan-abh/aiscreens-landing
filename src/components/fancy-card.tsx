44a06bd1dfd8e563c7e4a63efe6371a1242f0488
arshaan-abh <arshaan.git@gmail.com>
2024-01-31 18:27:02 +0330

    add fancy card

---
+++ src/components/fancy-card.tsx
@@ -0,0 +1,19 @@
+import type { FC } from "react";
+import HTMLProps from "@src/interfaces/html-props";
+
+const FancyCard: FC<HTMLProps<HTMLDivElement>> = (props) => {
+  const { className, refProp, children, ...otherProps } = props;
+  return (
+    <div
+      className={`grid rounded-2xl bg-gradient-to-r from-cyan-300 to-cyan-950 p-px ${className}`}
+      ref={refProp}
+      {...otherProps}
+    >
+      <div className="rounded-[calc(1rem-1px)] bg-gradient-to-r from-zinc-950 to-teal-950">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default FancyCard;
