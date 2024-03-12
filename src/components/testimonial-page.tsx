237e958d373201eaa595d9dff14859e13687b587
arshaan-abh <arshaan.git@gmail.com>
2024-03-12 21:45:16 +0330

    add testimonial

--- src/components/testimonial-page.tsx
+++ src/components/testimonial-page.tsx
@@ -1,10 +1,37 @@
-import type { FC } from "react";
-import MapComponent from "./map";
+"use client";
+import { useCallback, useState, type FC } from "react";
+import { ImageBorder, NextSlide, PrevSlide, Quotes } from "./icons";
+import TestimonialSlider from "./testimonial-slider";
+import { CarouselApi } from "./carousel";
 
 const TestimonialPage: FC = () => {
+  const [api, setApi] = useState<CarouselApi>();
+
+  const nextHandler = useCallback(() => {
+    api?.scrollNext();
+  }, [api]);
+
+  const prevHandler = useCallback(() => {
+    api?.scrollPrev();
+  }, [api]);
+
   return (
-    <div className="grid-cols-[repeat(16,1fr)] grid-rows-5">
-      <MapComponent className="h-full w-full" />
+    <div className="relative mt-4 flex flex-col items-center">
+      <div className="absolute inset-0 z-10">
+        <TestimonialSlider setApi={setApi} />
+      </div>
+      <div className="absolute inset-0 flex flex-col items-center justify-between">
+        <div className="flex items-center">
+          <button onClick={prevHandler} className="z-10">
+            <PrevSlide />
+          </button>
+          <ImageBorder />
+          <button onClick={nextHandler} className="z-10">
+            <NextSlide />
+          </button>
+        </div>
+        <Quotes className="ml-auto text-primary-600 opacity-50" />
+      </div>
     </div>
   );
 };
---
+++ src/components/testimonial-slider.tsx
@@ -0,0 +1,91 @@
+import type { Dispatch, FC, SetStateAction } from "react";
+import { Carousel, CarouselContent, CarouselItem } from "./carousel";
+import person1 from "/public/person-1.jpg";
+import { StaticImageData } from "next/image";
+import Image from "next/image";
+import { Star, StarFill } from "./icons";
+
+interface Testimonial {
+  image: StaticImageData;
+  name: string;
+  job: string;
+  content: string;
+  star: 0 | 1 | 2 | 3 | 4 | 5;
+}
+
+type Testimonials = Testimonial[];
+
+const testimonials: Testimonials = [
+  {
+    image: person1,
+    name: "Rachel A",
+    job: "Marketing Manager",
+    content:
+      "Using AiScreens for our marketing campaigns has been a game-changer. It's like having a virtual marketing strategist working around the clock!",
+    star: 4,
+  },
+  {
+    image: person1,
+    name: "John M.",
+    job: "Small Business Owner",
+    content:
+      "As a small business owner, AiScreens has allowed me to compete with larger companies on a more level playing field. It has helped me reach my target audience more effectively, resulting in higher ROI for my advertising budget.",
+    star: 5,
+  },
+  {
+    image: person1,
+    name: "Emily L.",
+    job: "Retail Store Owner",
+    content:
+      "AiScreens is one of the most innovative tools I've come across. It allows us to stay ahead of market trend sand adjust our campaigns in real-time, ensuring maximum impact. Plus, the intuitive interface makes it easy to navigate and optimize our campaigns for success.",
+    star: 4,
+  },
+];
+
+interface TestimonialSliderProps {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  setApi: Dispatch<SetStateAction<any>>;
+}
+
+const TestimonialSlider: FC<TestimonialSliderProps> = ({ setApi }) => {
+  return (
+    <Carousel setApi={setApi}>
+      <CarouselContent className="-ml-8">
+        {testimonials.map((testimonial, index) => (
+          <CarouselItem className="pl-8" key={index}>
+            <div className="flex w-full flex-col items-center gap-4">
+              <Image
+                className="mt-[13px] rounded-lg"
+                src={testimonial.image}
+                alt={testimonial.name}
+              />
+              <div className="flex flex-col">
+                <h4 className="text-center text-2xl font-bold">
+                  {testimonial.name}
+                </h4>
+                <p className="text-center text-sm text-primary-700">
+                  {testimonial.job}
+                </p>
+              </div>
+              <p className="w-[368px] text-center text-base">
+                Using AiScreens for our marketing campaigns has been a
+                game-changer. It&#39;s like having a virtual marketing
+                strategist working around the clock!
+              </p>
+              <div className="flex flex-row gap-1 text-primary-700">
+                {Array.from(Array(testimonial.star), (_, index) => (
+                  <StarFill key={index} />
+                ))}
+                {Array.from(Array(5 - testimonial.star), (_, index) => (
+                  <Star key={index} />
+                ))}
+              </div>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  );
+};
+
+export default TestimonialSlider;
